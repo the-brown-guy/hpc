560cc8483d55fa56343c404a1a1fb263e02a43c8
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-12-15 22:50:19 +0530

    Add files via upload

---
+++ p6.cpp
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    srand(static_cast<unsigned>(time(0)) + world_rank); // or, srand((unsigned int)time(0) + world_rank);
+    int mangoes_picked = rand() % 101;
+    cout << "Robot " << world_rank << " picked " << mangoes_picked << " mangoes." << endl;
+    int total_mangoes;
+    MPI_Reduce(&mangoes_picked, &total_mangoes, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (world_rank == 0)
+    {
+        cout << "Total mangoes picked by all robots: " << total_mangoes << endl;
+    }
+    MPI_Finalize();
+    return 0;
+}
