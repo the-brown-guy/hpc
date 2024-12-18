d309d25df3c4b1bf986aeed6b37cb16225e7c381
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-12-18 14:20:00 +0530

    Update p6.cpp

--- p6.cpp
+++ p6.cpp
@@ -7,9 +7,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
-    int world_size;
+    int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     srand((unsigned int)time(0) + world_rank); // or, srand(static_cast<unsigned>(time(0)) + world_rank);
     int mangoes_picked = rand() % 101;
